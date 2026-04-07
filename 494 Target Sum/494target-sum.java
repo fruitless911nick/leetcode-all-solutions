5c336cde089844f414e1ead08ecd7f238b9cd1cd
fruitless911nick <satyamsrivatav2003@gmail.com>
2026-04-07 11:32:20 +0000

    Code challenge submission for question: 494 Target Sum

---
+++ 494 Target Sum/494target-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return helper(0,target,nums);
+    }
+    static int helper(int i,int target,int[] nums){
+        if(i==nums.length){
+            if(target==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        int add=helper(i+1,target-nums[i],nums);
+        int subtract=helper(i+1,target+nums[i],nums);
+        return add+subtract;
+    }
+}
