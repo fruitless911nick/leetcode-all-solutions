d9c53b9dd5797dc7548069fa06b05b99675284c5
fruitless911nick <satyamsrivatav2003@gmail.com>
2026-04-13 01:34:11 +0000

    Code challenge submission for question: 516 Longest Palindromic Subsequence

---
+++ 516 Longest Palindromic Subsequence/516longest-palindromic-subsequence.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        StringBuilder a= new StringBuilder(s);
+        StringBuilder b=new StringBuilder(s);
+        b.reverse();
+        return lcs(a,b);
+    }
+
+    public int lcs(StringBuilder a, StringBuilder b) {
+        int m= a.length(),n=b.length();
+        int[][] dp=new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j]=-1;
+            }
+        }
+        return LCS(m-1,n-1,a,b,dp);
+    }
+
+    public int LCS(int i, int i1, StringBuilder a, StringBuilder b, int[][] dp) {
+        if(i<0 || i1<0){
+            return 0;
+        }
+        if(dp[i][i1]!=-1){
+            return dp[i][i1];
+        }
+        if(a.charAt(i)==b.charAt(i1)){
+            return 1+ LCS(i-1,i1-1,a,b,dp);
+        }
+        else{
+            return dp[i][i1]=Math.max(LCS(i-1,i1,a,b,dp),LCS(i,i1-1,a,b,dp));
+        }
+    }
+}
